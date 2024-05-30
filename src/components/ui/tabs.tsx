575f4679e7eaa1e6730690485dd91000a67d8474
Abhijit Mahato <abhijit.mahato.dev@gmail.com>
2024-05-31 01:00:51 +0530

    fix small bugs

--- src/components/ui/tabs.tsx
+++ src/components/ui/tabs.tsx
@@ -24,16 +24,16 @@ const TabsList = React.forwardRef<
 TabsList.displayName = TabsPrimitive.List.displayName;
 
 const triggerVariants = cva(
-  "inline-flex items-center justify-center whitespace-nowrap rounded-sm px-3 py-1.5 text-sm font-medium ring-offset-background transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 data-[state=active]:shadow-sm",
+  "inline-flex items-center justify-center whitespace-nowrap rounded-sm px-3 py-1.5 text-sm font-medium ring-offset-background transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 data-[state=active]:shadow-sm hover:text-foreground",
   {
     variants: {
       variant: {
         default:
-          "data-[state=active]:bg-background data-[state=active]:text-foreground data-[state=active]:hover:bg-background/90 data-[state=active]:hover:text-foreground",
+          "data-[state=active]:bg-background data-[state=active]:text-foreground data-[state=active]:hover:bg-background/80",
         primary:
-          "data-[state=active]:bg-primary data-[state=active]:text-primary-foreground data-[state=active]:hover:text-primary-foreground data-[state=active]:hover:bg-primary/90 hover:text-foreground",
+          "data-[state=active]:bg-primary data-[state=active]:text-primary-foreground data-[state=active]:hover:text-primary-foreground data-[state=active]:hover:bg-primary/80",
         "primary-left":
-          "data-[state=active]:bg-background data-[state=active]:text-foreground data-[state=active]:hover:bg-background/90 data-[state=active]:hover:text-foreground data-[state=active]:border-l-2 data-[state=active]:border-primary transition-none",
+          "data-[state=active]:bg-background data-[state=active]:text-foreground data-[state=active]:hover:bg-background/80 data-[state=active]:border-l-2 data-[state=active]:border-primary transition-none",
       },
     },
     defaultVariants: {
